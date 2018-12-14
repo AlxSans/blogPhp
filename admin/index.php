d492b334e9c8ef7ad58c77eaa263c59a250acbaa
Alejandro Sandoval <alxsan@outlook.com>
2018-12-13 23:47:15 -0600

    index de la carpeta admin

---
+++ admin/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Blog con PHP</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+  	<div class="container">
+      <div class="row">
+        <div class="col-md-12">
+
+        	<h1>Blog Title</h1>
+        	<div class="row">
+
+        		<div class="col-md-8">
+
+               <ul>
+                 <li>
+                   <a href="posts.php">Manage Posts</a>
+                 </li>
+               </ul>
+
+            </div>
+        		<div class="col-md-4">
+              <h2>Side Bar</h2>
+        			 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        		</div>
+
+        	
+
+         
+				
+        </div>
+         <div class="row">
+            <footer>
+              This is a footer
+            </footer>
+          </div>
+      </div>
+
+
+  	</div>
+</body>
+</html>
